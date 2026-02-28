6fcdf5ab0fb86eb097a85c0b57f9361896902bea
napppkun <napaphat.khun@gmail.com>
2026-03-01 03:35:55 +0700

    sort collect_3 array

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello, world!");
+// test sort collection_3
+import { sortArray } from './sortArray.js'
+
+const collection_3 : number[] = [2, 1, 0]
+const sorted_collection_3 = sortArray(collection_3)
+console.log('sorted collection_3: ', sorted_collection_3)
---
+++ src/mergeFunction.ts
@@ -0,0 +1,2 @@
+//import type { IMerge } from './IMerge.js';
+
