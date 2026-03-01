e37d1d63643a81d712cf91baf413b0df5b54e1cf
napppkun <napaphat.khun@gmail.com>
2026-03-01 19:35:20 +0700

    rename sortArray to reverseArray

--- src/reverseArray.ts
+++ src/reverseArray.ts
@@ -1,8 +1,8 @@
-export function sortArray (collection: number[]): number[] {
+export function reverseArray (collection: number[]): number[] {
     const len = collection.length
     const sorted: number[] = new Array(len).fill(0)
     // collection_3 already sorted from max to min(0)
-  // only need to reverse it to min to max 
+    // only need to reverse it to min to max 
     for (let i=0; i < len; i++) {
         sorted[i] = collection[len - 1 - i] as number // avoid undefined type error
     }
